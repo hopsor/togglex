b37886bbc31384ff926353a509ac9f62122c9fbe
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2016-11-18 15:44:23 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Togglex.Mixfile do
   def project do
     [
       app: :togglex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
